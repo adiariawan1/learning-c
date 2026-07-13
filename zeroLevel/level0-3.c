5bfa05b01874f82349ac288c106c1d8d3601c832
adiariawan1 <adiariawan1140@gmail.com>
2026-07-13 16:58:47 +0800

    fix zero level no 3

---
+++ zeroLevel/level0-3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (){
+    // looping for ping start from 0 until 10 and variable angka 
+    for(int angka = 0; angka <=10; angka++){
+        printf("ping attempt %d\n", angka);
+    }
+}
