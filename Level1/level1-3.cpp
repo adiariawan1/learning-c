b8da05c2290429996ccaed7829f55f2a130e4dda
adiariawan1 <adiariawan1140@gmail.com>
2026-07-24 23:03:25 +0800

    update level 1 step 3 and 4

--- Level1/level1-3.cpp
+++ Level1/level1-3.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 
 using std :: cout;
-
+int count = 0;
 int main(){
+    int data[15]={-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,12,-13,-14,15};
     
+    for(int step = 0; step < 15; step++){
+        if(data[step] > 0){
+            count += 1;
+        }
+    }
+    cout<<count;
     return 0;
 }
---
+++ Level1/level1-4.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using std :: cout;
+
+int main(){
+    for(int number = 5; number <= 50; number += 5){
+        cout <<number<<' ';
+    }
+}
